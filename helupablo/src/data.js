3aa5720ccfd9f197635c8e2fb5f28f0c7a1278c9
HeluPablo <pablo_andaas@hotmail.com>
2022-11-05 04:48:50 -0300

    mt-f1-01

---
+++ helupablo/src/data.js
@@ -0,0 +1,166 @@
+let datos = [{
+    id: "user1",
+    name: "Pablo",
+    lastName: "Helu",
+    age: "34",
+    email: "pablohelu@hotmail.com",
+    password: "123456",
+    code: "5465354",
+    verified: true,
+    logged: true
+},
+
+{
+    id: "user2",
+    name: "Ezequiel",
+    lastName: "Quatrocchi",
+    age: "33",
+    email: "ezequat@hotmail.com",
+    password: "qwerty2021",
+    code: "5465435",
+    verified: true,
+    logged: true
+},
+
+{
+    id: "user3",
+    name: "Adrián",
+    lastName: "Parga",
+    age: "32",
+    email: "adripar@hotmail.com",
+    password: "654321",
+    code: "5465486",
+    verified: true,
+    logged: true
+},
+
+{
+    id: "user4",
+    name: "Manuel",
+    lastName: "Pires",
+    age: "28",
+    email: "manupir@hotmail.com",
+    password: "123458",
+    code: "5453135",
+    verified: true,
+    logged: true
+}]
+
+let ciudades = [{
+    id: "city1",
+    name: "Bangkok",
+    continent: "Asia",
+    photo: "/public/img/bangkok.jpg",
+    population:  14626225,
+    userId:  user1
+},
+
+{
+    id: "city2",
+    name: "París",
+    continent: "Europa",
+    photo: "/public/img/paris.jpg",
+    population:  2161000,
+    userId:  user1
+},
+
+{
+    id: "city3",
+    name: "Londres",
+    continent: "Europa",
+    photo: "/public/img/londres.jpg",
+    population:  8982000,
+    userId:  user1
+},
+
+{
+    id: "city4",
+    name: "Dubái",
+    continent: "Asia",
+    photo: "/public/img/dubai.jpg",
+    population:  3331000,
+    userId:  user1 
+},
+
+{
+    id: "city5",
+    name: "Singapur",
+    continent: "Asia",
+    photo: "/public/img/singapur.jpg",
+    population:  5677000,
+    userId:  user1 
+},
+
+{
+    id: "city6",
+    name: "Kuala Lumpur",
+    continent: "Asia",
+    photo: "/public/img/kuala.jpg",
+    population:  1808000,
+    userId:  user1
+},
+
+{
+    id: "city7",
+    name: "Nueva York",
+    continent: "América",
+    photo: "/public/img/nuevayork.jpg",
+    population:  8468000,
+    userId:  user1
+},
+
+{
+    id: "city8",
+    name: "Tokio",
+    continent: "Asia",
+    photo: "/public/img/tokio.jpg",
+    population:  13960000,
+    userId:  user1
+},
+
+{
+    id: "city9",
+    name: "Antalya",
+    continent: "Asia",
+    photo: "/public/img/antalya.jpg",
+    population:  2619832,
+    userId:  user1
+},
+
+{
+    id: "city10",
+    name: "Seúl",
+    continent: "Asia",
+    photo: "/public/img/seul.jpg",
+    population:  9776000,
+    userId:  user1
+},
+
+{
+    id: "city11",
+    name: "Osaka",
+    continent: "Asia",
+    photo: "/public/img/osaka.jpg",
+    population:  2691000,
+    userId:  user1
+},
+
+{
+    id: "city12",
+    name: "Meca",
+    continent: "Asia",
+    photo: "/public/img/meca.jpg",
+    population:  1579000,
+    userId:  user1
+}]
+
+/* let atracciones = [{ X6
+    id: it1,
+    citiId: city1 (id de la ciudad a la que pertenece),
+    name: Monumento a la Bandera,
+    photo: (array con 3 URL de fotos),
+    description; It is located in the place where Belgrano raised the Argentine flag for the first time, on the banks of the Paraná River.
+    price: 10 (la moneda USD se escribe en el front),
+    duration: 1 (el tiempo hs se escibe en el front),
+    userId:  user1 (id de un usuario registrado),
+}] */
