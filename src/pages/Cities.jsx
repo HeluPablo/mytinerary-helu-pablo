3847c332fb57093e0e73b9abba9a85a8d6c2f1bf
HeluPablo <pablo_andaas@hotmail.com>
2022-11-21 18:58:15 -0300

    Update Cities.jsx

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -12,8 +12,12 @@ export default function Cities() {
   
   useEffect(() => {
     axios.get(`http://localhost:8000/api/cities`)
-    .then((res) => setCheckBox(res.data.response))
-    .then((res) => setCiudades(res.data.response));
+    .then((res) => {
+      setCheckBox(res.data.response)
+      setCiudades(res.data.response)
+    })
+    
+    ;
     
     /* axios.get(`http://localhost:8000/api/cities`)
     .then((res) => ); */
