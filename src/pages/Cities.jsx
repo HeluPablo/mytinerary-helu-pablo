4f34ef53ced37fa9a6fee8a2d34e51a0693eccc4
HeluPablo <pablo_andaas@hotmail.com>
2022-11-26 04:20:10 -0300

    Update Cities.jsx

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,65 +1,83 @@
-import React from 'react'
-import CardCity from '../components/CardCity'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React from "react";
+import CardCity from "../components/CardCity";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Cities() {
-  const [search, setSearch] = useState("")
-  let [ciudades , setCiudades] = useState()
-  let [checkbox , setCheckBox] = useState()
-  
-  
+  const [search, setSearch] = useState("");  ///Cambia el valor al estado 
+  let [ciudades, setCiudades] = useState();
+  let [checkbox, setCheckBox] = useState();
+  let [check, setCheck] = useState([]);
+
   useEffect(() => {
-    axios.get(`http://localhost:8000/api/cities`)
-    .then((res) => {
-      setCheckBox(res.data.response)
-      setCiudades(res.data.response)
-    })
-    
-    ;
-    
-    /* axios.get(`http://localhost:8000/api/cities`)
-    .then((res) => ); */
-    
-  }, [])
+    axios
+      .get(`http://localhost:8000/api/cities`) /// Petición de traer los checkbox
+      .then((res) => {
+        setCheckBox(res.data.response);
+      });
+
+    axios
+      .get(`http://localhost:8000/api/cities`, {
+        params: {
+          continent: check,
+          name: search,
+        },
+      }) /// Petición para filtrar y traer las ciudades
+      .then((res) => setCiudades(res.data.response));
+  }, [check, search]);
+
+  const checkBoxArray = Array.from(new Set(checkbox?.map((e) => e.continent)));
+
+  // console.log(checkBoxArray)
 
   const searcher = (e) => {
-    setSearch(e.target.value)
-    console.log(search)
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  function onClickCheckBox(e) {
+    console.log(e.target.value);
+    let checkedCheckBox = [];
+
+    if (e.target.checked) {
+      checkedCheckBox = [...check, e.target.value];
+    } else {
+      checkedCheckBox = check.filter((i) => e.target.value !== i);
+    }
+    setCheck(checkedCheckBox);
   }
 
-function checked(e){
-  console.log(e.target.value)
-}
-
-
-
-
-
   return (
-    <div className='body'>
-      <div className='input-cont'>
+    <div className="body">
+      <div className="input-cont">
         <input
           className="inputsearch form-control"
           placeholder="search"
           value={search}
           onChange={searcher}
         />
-        <div className="topping">
-
-          {checkbox?.map((e) => (
-            <>
-            <label className="Checkbox-labels" for={e.name}>{e.continent}</label>
-            <input type="checkbox" value={e.name} onClick={checked} ></input>            
-            </>            
+        <div className="input-cont">
+          {checkBoxArray?.map((e) => (
+            <div key={e}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={e}
+                  onClick={onClickCheckBox}
+                ></input>
+                {e}
+              </label>
+            </div>
           ))}
-
         </div>
       </div>
-      <div className='card-cont'>
-        {ciudades?.map(ciudad => <CardCity key={ciudad.id}{...ciudad}/>)} 
+      <div className="card-cont">
+        {ciudades?.map((city) => (
+          <div key={city.name}>
+            <CardCity key={city.id} {...city} />
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
