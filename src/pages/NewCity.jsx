cb45dc3b66dd2d268091071065bb341b027f241b
HeluPablo <pablo_andaas@hotmail.com>
2022-11-28 03:35:35 -0300

    Update NewCity.jsx

--- src/pages/NewCity.jsx
+++ src/pages/NewCity.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
 
 export default function NewCity() {
     const handleSubmit = (event) => {
         event.preventDefault();
+        
         const userData = {
             name: event.target[0].value,
             surname: event.target[1].value,
@@ -10,9 +15,30 @@ export default function NewCity() {
             password: event.target[3].value,
         }
         console.log(userData)
+
+        confirmAlert({
+            title: 'New city',
+            message: 'create new city?.',
+            buttons: [
+              {
+                label: 'Yes',
+                onClick: () => toast('city created succesfully')
+              },
+              {
+                label: 'No',
+                onClick: () => console.log('Click No')
+              }
+            ]
+          });
     }
+    
+
+
+
+
   return (
     <div className='body-form'>
+              <ToastContainer />
         <section className='form-cont'>
                 <h2 className='form-title'>Register a new city</h2>
                 <form onSubmit={handleSubmit}>
