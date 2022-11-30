0b644cf9ddbdcde2293a3a3d99c082fff48d0aa6
HeluPablo <pablo_andaas@hotmail.com>
2022-11-30 06:31:38 -0300

    Update Cities.jsx

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import CardCity from "../components/CardCity";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Alerts from "../components/Alerts";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
+import { useGetAllQuery } from "../Redux/reducers/citiesAPI";
 
 export default function Cities() {
-  // toast("Probando =) ")
-
-
-  const [search, setSearch] = useState("");  ///Cambia el valor al estado 
-  let [ciudades, setCiudades] = useState();
+  const [search, setSearch] = useState(""); ///Cambia el valor al estado
+  // let [cities, setCities] = useState();
   let [checkbox, setCheckBox] = useState();
   let [check, setCheck] = useState([]);
+
+  let {data : redux} = useGetAllQuery({search , check})
+
+console.log(redux?.response)
+let cities = redux?.response
 
   useEffect(() => {
     axios
@@ -23,16 +24,18 @@ export default function Cities() {
         setCheckBox(res.data.response);
       });
 
-    axios
-      .get(`http://localhost:8000/api/cities`, {  /// Petición para filtrar y traer las ciudades
-        params: {
-          continent: check,
-          name: search,
-        },
-      })
-      .then((res) => setCiudades(res.data.response));
+    // axios
+    //   .get(`http://localhost:8000/api/cities`, {  /// Petición para filtrar y traer las ciudades
+    //     params: {
+    //       continent: check,
+    //       name: search,
+    //     },
+    //   })
+    //   .then((res) => setCiudades(res.data.response));
+
+    // getAllRedux().then((res) => console.log(res));
   }, [check, search]);
-  
+
   const checkBoxArray = Array.from(new Set(checkbox?.map((e) => e.continent)));
 
   // console.log(checkBoxArray)
@@ -80,11 +83,11 @@ export default function Cities() {
         </div>
       </div>
       <div className="card-cont">
-        {ciudades?.map((city) => (
+        {cities?.map((city) => (
           <div key={city.name}>
             <CardCity key={city.id} {...city} />
           </div>
-        ))}      
+        ))}
       </div>
     </div>
   );
