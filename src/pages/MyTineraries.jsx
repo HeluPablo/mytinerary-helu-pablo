db17804b7f5e5258e8b8a9ab8c357a37286c9dac
HeluPablo <pablo_andaas@hotmail.com>
2022-12-01 06:17:29 -0300

    Create MyTineraries.jsx

---
+++ src/pages/MyTineraries.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Itinerary from '../components/Itinerary'
+
+export default function MyTineraries() {
+
+
+
+  return (
+
+<>
+    <div>MyTineraries</div>
+    <Itinerary CityId = {""} userId = {"6375e7b7a417c13e23b73296"}/>
+</>
+    
+
+    
+  )
+}
